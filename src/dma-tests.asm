9fb70e78fd0197efbcc806fca5739c3bdab26519
Paul Wightmore <paul.wightmore@virginmedia.com>
2025-07-30 22:42:31 +0100

    Refactored DMA macros to handle optional parameters. Fleshed out some unit tests for the macros. N.b. I have not attempted to compile *anything* yet, so there may be syntax errors or other issues.

--- src/dma-tests.asm
+++ src/dma-tests.asm
@@ -37,9 +37,83 @@ ENDT
 ;    ret
 ;ENDT
 
-; Table of tests for this module
+; -----------------------------------------------------------
+; DMA_WR0 Macro Unit Tests
+; -----------------------------------------------------------
+
+TEST DMA_WR0_AllParams
+    ; Should emit base + 4 bytes
+    wr0_all_start:
+        DMA_WR0 0x01, 0x02, 0x03, 0x04
+    wr0_all_end:
+    ld hl, wr0_all_start
+    ld de, wr0_all_expected
+    ld bc, wr0_all_end-wr0_all_start
+    call assert_mem_equal
+    ret
+ENDT
+
+wr0_all_expected:
+    db ((1<<1)|(1<<2)|(1<<3)|(1<<4))    ; base flags: all present
+    db 0x01, 0x02, 0x03, 0x04
+
+TEST DMA_WR0_SomeParams
+    ; Only PA_MSB and BL_MSB present
+    wr0_some_start:
+        DMA_WR0 -1, 0xAA, -1, 0xBB
+    wr0_some_end:
+    ld hl, wr0_some_start
+    ld de, wr0_some_expected
+    ld bc, wr0_some_end-wr0_some_start
+    call assert_mem_equal
+    ret
+ENDT
+
+wr0_some_expected:
+    db ((1<<2)|(1<<4))    ; base flags: PA_MSB and BL_MSB
+    db 0xAA, 0xBB
+
+TEST DMA_WR0_None
+    ; No parameters present
+    wr0_none_start:
+        DMA_WR0 -1, -1, -1, -1
+    wr0_none_end:
+    ld hl, wr0_none_start
+    ld de, wr0_none_expected
+    ld bc, wr0_none_end-wr0_none_start
+    call assert_mem_equal
+    ret
+ENDT
+
+wr0_none_expected:
+    db 0x00    ; base flags: none present
+
+; Add these to the test table:
 TestsTable:
     dw DMA_Test_Transfer
-    ;dw DMA_Test_Another
+    dw DMA_WR0_AllParams
+    dw DMA_WR0_SomeParams
+    dw DMA_WR0_None
 
 NumTests: equ ($ - TestsTable) / 2
+
+; ResultsTable holds the result status for each test:
+;   0 = not run
+;   1 = pass
+;   2 = fail
+;   3 = running
+ResultsTable:
+    ds NumTests, 0    ; Reserve NumTests bytes,
+
+; In your test runner, before running tests:
+    ld hl, ResultsTable
+    ld b, NumTests
+.clear_results:
+    ld (hl), 0
+    inc hl
+    djnz .clear_results
+
+; After each test, set result:
+;   ld hl, ResultsTable
+;   add hl, test_index
+;   ld (hl), 1    ; 1 = pass, 2 = fail
